c3c6088cedcfa671a27dc6fd92559223f32c73cc
Auditi Sinha <150148214+aditi-190@users.noreply.github.com>
2026-06-09 16:17:10 +0600

    Update Problem2.py

--- Task 2/Problem2.py
+++ Task 2/Problem2.py
@@ -1,1 +1,17 @@
 
+def most_kmers(text,k):
+    freq = {}
+    
+    for i in range(len(text)-k+1):
+        kmer = text[i:i+k]
+        freq[kmer]=freq.get(kmer, 0) +1
+    max_count = max(freq.values())
+    result=[]
+    for kmer in freq:
+            if freq[kmer] == max_count:
+                result.append(kmer)            
+    return result
+text = input().strip()
+k = int(input())
+
+print(*most_kmers(text,k))
