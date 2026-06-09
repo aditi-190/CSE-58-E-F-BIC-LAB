daa5c39325977298860a45aa502b3b45dfe04823
Auditi Sinha <150148214+aditi-190@users.noreply.github.com>
2026-06-09 16:22:14 +0600

    Update Problem3.py

--- Task 2/Problem3.py
+++ Task 2/Problem3.py
@@ -1,1 +1,15 @@
 
+def reverse_complement(dna):
+    comp={
+        'A':'T',
+        'T':'A',
+        'C':'G',
+        'G':'C'     
+        }
+    result=""
+    for ch in dna:
+        result+=comp[ch]
+    return result[::-1]
+dna=input().strip()
+
+print(reverse_complement(dna))
