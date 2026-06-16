31992fbfc21c7a30008508563898a511b88ef705
Auditi Sinha <150148214+aditi-190@users.noreply.github.com>
2026-06-16 16:19:19 +0600

    Update Problem2.py

--- Task 3/Problem2.py
+++ Task 3/Problem2.py
@@ -1,1 +1,21 @@
 
+def hamming(a,b):
+    return sum(1 for i in range (len(a)) if a[i] != b[i])
+
+def a_p_m(pattern, text, d):
+    k=len(pattern)
+    positions=[]
+    
+    for i in range(len(text)-k+1):
+        window = text[i:i+k]
+        if hamming(pattern,window) <= d:
+            positions.append(i)
+            
+    return positions
+
+pattern = input().strip()
+text=input().strip()
+d=int(input().strip())
+
+result = a_p_m(pattern,text,d)
+print(*result)
